635b5822c8e1a4dbde05cb9431d1ed382d59512f
你的用户名 <>
2024-09-04 22:53:17 +0800

    update

--- CompileAgent.py
+++ CompileAgent.py
@@ -13,7 +13,7 @@ from langchain.agents import AgentExecutor,Tool
 from langchain_openai import ChatOpenAI
 from langchain import hub
 from langchain.prompts import PromptTemplate
-from tools import InteractiveDockerShell, doc_sum, makefile_reader
+from tools import InteractiveDockerShell, SearchCompilationInstruction, SearchCompilationTarget, doc_sum, makefile_reader
 from config import *
 logging.basicConfig(level=logging.INFO, format='%(asctime)s | %(name)s | %(levelname)s | %(message)s')
 logging.getLogger('httpx').setLevel(logging.ERROR)
@@ -21,6 +21,7 @@ logging.getLogger('httpx').setLevel(logging.ERROR)
 os.environ["LANGCHAIN_TRACING_V2"] = LANGCHAIN_TRACING_V2
 os.environ["LANGCHAIN_API_KEY"] = LANGCHAIN_API_KEY
 os.environ["LANGCHAIN_PROJECT"] = LANGCHAIN_PROJECT
+
 
 llm = ChatOpenAI(
     base_url="https://api.fireworks.ai/inference/v1",
@@ -35,39 +36,55 @@ question_template = """You are an experienced software development engineer. I w
 Please complete the compilation task step by step, use the tools provided if require (one tool each time).
 
 The project has been downloaded into /work/, which is also the SHELL's initial path.
-The following information could be helpful:
-1. Use `tree <dir> -L <depth>` in SHELL to check the directory structure.
-2. Find out the compilation guide using DOC_SUM tool to summarize the files that you consider them as documentations.
-3. Use `cat` in SHELL to check the content of any file from the project.
-4. Do not install the project, just compile it.
-5. Make sure that compile the project in the right path.
-6. Use the MAKEFILE_READER tool to find out all the compilation targets defined in the makefile after the compilation.
-7. To verify the compilation result, you need to check whether the target files exist, using `find` in SHELL, and you need to output COMPILATION-SUCCESS, COMPILATION-FAIL, or COMPILATION-UNCERTAIN as the final answer.
+The following information must be followed:
+1. Use the SHELL tool to execute `pwd` command to make sure you are in the right path.
+2. Do not install the project and don't test the project, just compile it.
+3. Use the SEARCH_INSTRUCTION tool to looking for instructions about how to compile the project and then use the SHELL tool to compile it.
+4. If there are any difficulties during compilation, try to resolve them.
+5. Find the project's Makefile file and use the SEARCH_TARGET tool to find out all the compilation targets defined in the makefile after the compilation.
+6. Use `find` in SHELL to check out if the files exist.
+7. After checking, you need to output COMPILATION-SUCCESS, COMPILATION-FAIL, or COMPILATION-UNCERTAIN as the final answer.
 """
 # You can read the README.md file in the project path to find compilation guide. 
 # If any problems occur during compilation, please use Google search tools to solve them first. 
 
 def to_compile_project():
     projects = {
-        "tiny-AES-c": {
-            "local_path": "/data/huli/CompileAgent/dataset/tiny-AES-c",
-            "url": "https://github.com/kokke/tiny-AES-c",
-            "files": [
-                "test.elf",
-            ],
-        },
+        # "jq": {
+        #     "url": "https://github.com/jqlang/jq",
+        #     "local_path": "/data/huli/CompileAgent/dataset/jq"
+        # },
+        "redis": {
+            "local_path": "/data/huli/CompileAgent/dataset/redis",
+            "url": "https://github.com/redis/redis"
+        }
+        # "tmux": {
+        #     "local_path": "/data/huli/CompileAgent/dataset/tmux",
+        #     "url": "https://github.com/tmux/tmux"
+        # },
+        # "tiny-AES-c": {
+        #     "local_path": "/data/huli/CompileAgent/dataset/tiny-AES-c",
+        #     "url": "https://github.com/kokke/tiny-AES-c",
+        #     "files": [
+        #         "test.elf",
+        #     ],
+        # },
     }
     # with open("projects.json","r") as file:
     #     projects = json.load(file)
     for proj_name,info in projects.items():
         yield (proj_name, info)
 
-def is_compiled(local_path, files):
-    if not files:
+def is_compiled(local_path, target_files):
+    if not target_files:
         return None
-    for file in files:
-        file_path = os.path.join(local_path,file)
-        if not os.path.exists(file_path):
+    file_list = []
+    for root, dirs, files in os.walk(local_path):
+        for file in files:
+            file_list.append(file)
+    for file in target_files:
+        file = os.path.basename(file)
+        if file not in file_list:
             return False
     return True
     
@@ -105,10 +122,14 @@ def save_logs(question,tools,res,local_path,ok):
     # logging process
     now = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S:%f")
     log_local_file = f"logs/log.{now}.txt"
+    logging.info(f"[-] Saving logs to {log_local_file}")
+    # breakpoint()
     with open(log_local_file,"w") as file:
         s = assemble_all_to_txt(template,tools,question,res["output"],res["intermediate_steps"])
         file.write(s)
-    with open(log_local_file.replace(".txt",".json"),"w") as file:
+    log_local_file_json = log_local_file.replace('.txt','.json')
+    logging.info(f"[-] Saving logs to {log_local_file_json}")
+    with open(log_local_file_json,"w") as file:
         s = assemble_all_to_json(template,tools,question,res["output"],res["intermediate_steps"])
         json.dump(s,file,indent='\t')
     # logging result
@@ -146,9 +167,10 @@ def copy_project(local_path):
         raise Exception(f"Failed to copy project from {local_path} to {new_path}")
     return new_path
 
+
 def main(
         dataset_base_path:str = None,
-        clear_new_project:bool = False,
+        clear_copied_project:bool = False,
         download_proxy:str = None,
         ):
     '''
@@ -156,7 +178,7 @@ def main(
     
     Args:
         dataset_base_path (str): the base path to store the projects need to compile
-        clear_new_project (bool): whether to clear the new project path after the compilation, set to True when debugging
+        clear_copied_project (bool): whether to clear the new project path after the compilation, set to True when debugging
         download_proxy (str): pass to git --config http.proxy=${}, e.g., socks5://127.0.0.1:9999, if do not set: do not use proxy
     '''
     DATASET_BASE_PATH = None
@@ -178,8 +200,11 @@ def main(
         # copy new project path for compilation
         local_path = copy_project(proj_path)
         os.environ["LOCAL_PATH"] = local_path
-
+        # breakpoint()
         with InteractiveDockerShell(local_path=local_path,timeout=120) as shell:
+            SearchIns = SearchCompilationInstruction(directory_path=local_path)
+            # SearchTar = SearchCompilationTarget(directory_path=local_path)
+            SearchTar = SearchTarget(directory_path=local_path)
             # define tools
             tools = [
                 Tool(
@@ -188,22 +213,22 @@ def main(
                     func=shell.execute_command
                 ),
                 Tool(
-                    name="DOC_SUM",
-                    description=doc_sum.__doc__,
-                    func=doc_sum
+                    name="SEARCH_INSTRUCTION",
+                    description=SearchIns.search_instruction.__doc__,
+                    func=SearchIns.search_instruction
                 ),
                 Tool(
-                    name="MAKEFILE_READER",
-                    description=makefile_reader.__doc__,
-                    func=makefile_reader
-                ),
+                    name="SEARCH_TARGET",
+                    description=SearchTar.search_target.__doc__,
+                    func=SearchTar.search_target
+                )
             ]
             # define agent
             agent = create_react_agent(llm=llm,tools=tools,prompt=prompt)
-            agent_executor = AgentExecutor(agent=agent,tools=tools,verbose=True,return_intermediate_steps=True,max_iterations=2,handle_parsing_errors=True)
+            agent_executor = AgentExecutor(agent=agent,tools=tools,verbose=True,return_intermediate_steps=True,max_iterations=50,handle_parsing_errors=True)
 
             res = None
-            question = question_template.format(url=url)
+            question = question_template.format(project_name=proj_name)
             try:
                 for step in agent_executor.iter({"input":question},include_run_info=True):
                     if "intermediate_step" in step:
@@ -222,9 +247,10 @@ def main(
             except Exception as e:
                 logging.error(f"[!] Compilation failed with error: {e}")
                 ok = False
+            # breakpoint()
             save_logs(question,tools,res,local_path,ok)
         
-        if clear_new_project:
+        if clear_copied_project:
             logging.info(f"[-] Clear project compilation path {local_path}")
             shutil.rmtree(local_path)
 
