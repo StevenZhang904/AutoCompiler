5d28d01059f93db4b41de2717b18b3c681f595ae
huli <pdxbshx@mail.ustc.edu.cn>
2025-05-24 16:20:15 +0800

    revised

--- CompileAgent.py
+++ CompileAgent.py
@@ -14,15 +14,20 @@ import pandas as pd
 from langchain.agents import create_react_agent
 from langchain.agents import AgentExecutor,Tool
 from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
 from langchain import hub
 from langchain.prompts import PromptTemplate
-from tools import InteractiveDockerShell, SearchCompilationInstruction, ReadmeAI
-from MultiAgentGetInstructions import GetInstructions
-from MultiAgentReconcile import Reconcile
-from CustomAgentExecutor import CompilationAgentExecutor
 import argparse
 import click
-from config import *
+
+from Tools import InteractiveDockerShell, SearchCompilationInstruction, ReadmeAI
+from CustomAgentExecutor import CompilationAgentExecutor
+from MultiAgentGetInstructions import CompileNavigator
+from MultiAgentDiscussion import ErrorSolver
+from DownloadProject import download_project, copy_project
+from Logs import is_compiled, save_logs
+from Config import *
+
 logging.basicConfig(level=logging.INFO, format='%(asctime)s | %(name)s | %(levelname)s | %(message)s')
 logging.getLogger('httpx').setLevel(logging.ERROR)
 logging.getLogger('chromadb').setLevel(logging.ERROR)
@@ -34,124 +39,7 @@ os.environ["LANGCHAIN_PROJECT"] = LANGCHAIN_PROJECT
 prompt = PromptTemplate.from_template(template)
 question_template = model_decision_template
 
-# def to_compile_project():
-#     with open("test_projects.json","r") as fp:
-#         projects = json.load(fp)
-#     assert len(set([pn["name"] for pn in projects]))==len(projects), "[!] Found duplicate project name"
-#     for item in projects:
-#         yield (item["name"], item)
-
-def is_elf(file):
-    with open(file,"rb") as f:
-        return f.read(4) == b"\x7fELF"
-    
-def is_archive(file):
-    with open(file, "rb") as f:
-        return f.read(8) in {b'!<arch>\n',b'!<thin>\n'}
-
-def is_compiled(local_path, target_files, strict=False):
-    if not target_files:
-        return None
-    elf_file_list = []
-    for root, dirs, files in os.walk(local_path):
-        for file in files:
-            fp = os.path.join(root,file)
-            if is_elf(fp) or is_archive(fp):
-                elf_file_list.append(file)
-    if strict: # only if all target files are in the file list
-        for file in target_files:
-            file = os.path.basename(file)
-            if file not in elf_file_list:
-                return False
-        return True
-    else: # if any target file is in the file list
-        for file in target_files:
-            file = os.path.basename(file)
-            if file in elf_file_list:
-                return True
-        return False
-
-def assemble_all_to_txt(template,tools,input,output,intermediate_steps):
-    tool_names = ", ".join([tool.name for tool in tools])
-    tool_info = "\n".join([f"{tool.name}: {tool.description}" for tool in tools])
-    agent_scratchpad = ""
-    for idx, (action, value) in enumerate(intermediate_steps):
-        model_response = action.log
-        tool_response = value
-        agent_scratchpad += "\nThought: " + model_response if idx!=0 else model_response
-        agent_scratchpad += "\nObservation: " + str(tool_response)
-    agent_scratchpad += "\nThought: " + output
-    result = template.format(tools=tool_info,tool_names=tool_names,input=input,agent_scratchpad=agent_scratchpad)
-    return result
-
-def assemble_all_to_json(template,tools,input,output,intermediate_steps):
-    tool_names = ", ".join([tool.name for tool in tools])
-    tool_info = "\n".join([f"{tool.name}: {tool.description}" for tool in tools])
-    init_question = template.format(tools=tool_info,tool_names=tool_names,input=input,agent_scratchpad="")
-    _pair = [init_question]
-    log_list = []
-    for idx, (action, value) in enumerate(intermediate_steps):
-        model_response = action.log
-        _pair.append(model_response)
-        log_list.append(_pair)
-        tool_response = value
-        _pair = [tool_response]
-    final_output = output
-    _pair.append(final_output)
-    log_list.append(_pair)
-    return log_list
-
-def save_logs(proj_name,question,tools,res,tools_logs,local_path,checker_ok,model_ok):
-    # logging process
-    now = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S:%f")
-    log_local_file = f"logs1/{proj_name}.log.{now}.txt"
-    logging.info(f"[-] Saving logs to {log_local_file}")
-    with open(log_local_file,"w") as file:
-        s = assemble_all_to_txt(template,tools,question,res["output"],res["intermediate_steps"])
-        file.write(s)
-    log_local_file_json = log_local_file.replace('.txt','.json')
-    logging.info(f"[-] Saving logs to {log_local_file_json}")
-    with open(log_local_file_json,"w") as file:
-        s = assemble_all_to_json(template,tools,question,res["output"],res["intermediate_steps"])
-        json.dump({"autocompiler":s, "tools":tools_logs},file,indent='\t')
-
-    # logging result
-    run_id = res["run_id"]
-    log_url = LOG_URL_TEMPLATE.format(run_id=run_id)
-    process_id = os.getpid()
-    with open(PROCESS_LOG_CSV_PATH+f"/{process_id}.csv","a") as file:
-        csv_writer = csv.writer(file)
-        csv_writer.writerow([run_id, log_url, log_local_file, local_path, model_ok, checker_ok])
-
-def download_project(url, local_path, download_proxy=None, timeout=120) -> bool:
-    logging.info(f"[-] Downloading project from {url} to {local_path}")
-    # use subprocess
-    if download_proxy!=None:
-        cmd = f"git clone {url} {local_path} --config http.proxy={download_proxy}"
-    else:
-        cmd = f"git clone {url} {local_path}"
-    logging.info(f"[-] Running command: {cmd}")
-    ret = subprocess.run(cmd,shell=True,capture_output=True)
-    # if ret.returncode == 128:
-    #     logging.info(f"[-] Project already exists in {local_path}")
-    #     return True
-    if ret.returncode != 0:
-        raise Exception(f"Failed to download project from {url}\n\n{ret.stderr.decode()}")
-    return True
-
-def copy_project(local_path):
-    UUID = uuid.uuid4()
-    local_path = os.path.abspath(local_path)
-    new_path = f"{local_path}-{UUID}"
-    cmd = f"chmod -R 777 {local_path} && cp -r {local_path} {new_path} && chmod -R 777 {new_path}"
-    logging.info(f"[-] Copy project from {local_path} to {new_path}")
-    ret = subprocess.run(cmd,shell=True,capture_output=True)
-    if ret.returncode != 0:
-        raise Exception(f"Failed to copy project from {local_path} to {new_path} {ret.stderr}")
-    return new_path
-
-
-def start_compile(dataset_base_path,clean_copied_project,download_proxy,strict_checker,retry,projects):
+def start_compile(dataset_base_path,log_path,clean_copied_project,download_proxy,strict_checker,retry,projects):
     DATASET_BASE_PATH = None
     if os.environ.get("DATASET_BASE_PATH"):
         DATASET_BASE_PATH = os.environ.get("DATASET_BASE_PATH")
@@ -162,7 +50,7 @@ def start_compile(dataset_base_path,clean_copied_project,download_proxy,strict_c
     if not os.path.exists(DATASET_BASE_PATH):
         os.makedirs(DATASET_BASE_PATH)
 
-    if download_proxy == None and PROXY != None:
+    if download_proxy != None:
         download_proxy=PROXY
 
     for info in projects:
@@ -178,72 +66,34 @@ def start_compile(dataset_base_path,clean_copied_project,download_proxy,strict_c
             os.environ["LOCAL_PATH"] = local_path
 
             llm = ChatOpenAI(
-                base_url=OPENAI_BASE_URL,
-                api_key=OPENAI_API_KEY,
-                model=OPENAI_MODEL,
+                base_url=DEEPSEEK_BASE_URL,
+                model=DEEPSEEK_MODEL,
+                api_key=DEEPSEEK_API_KEY,
                 temperature=1,
+                timeout=120
                 # http_client=httpx.Client(proxies=PROXY) if PROXY else None
             )
-            with InteractiveDockerShell(local_path=local_path,cmd_timeout=1200) as shell:
+            with InteractiveDockerShell(local_path=local_path,cmd_timeout=1200,use_proxy=True) as shell:
                 # define tools
-                if question_template == model_decision_template:
-                    get_ins = GetInstructions(project_name=proj_name)
-                    rec = Reconcile()
-                    tools = [
-                        Tool(
-                            name="SHELL",
-                            description=shell.execute_command.__doc__,
-                            func=shell.execute_command
-                        ),
-                        Tool(
-                            name="GET_INSTRUCTIONS",
-                            description=get_ins.get_instructions.__doc__,
-                            func=get_ins.get_instructions
-                        ),
-                        Tool(
-                            name="RECONCILE",
-                            description=rec.reconcile.__doc__,
-                            func=rec.reconcile
-                        )
-                    ]
-                if question_template == rag_template:
-                    search_inst_tool = SearchCompilationInstruction(directory_path=local_path, project_name=proj_name, use_proxy=PROXY)
-                    tools = [
-                        Tool(
-                            name="SHELL",
-                            description=shell.execute_command.__doc__,
-                            func=shell.execute_command
-                        ),
-                        Tool(
-                            name="SEARCH_INSTRUCTION_FROM_FILES",
-                            description=search_inst_tool.search_instruction_from_files.__doc__,
-                            func=search_inst_tool.search_instruction_from_files
-                        ),
-                        Tool(
-                            name="SEARCH_URL_FROM_FILES",
-                            description=search_inst_tool.search_url_from_files.__doc__,
-                            func=search_inst_tool.search_url_from_files
-                        ),
-                        Tool(
-                            name="SEARCH_INSTRUCTION_FROM_URL",
-                            description=search_inst_tool.search_instruction_from_url.__doc__,
-                            func=search_inst_tool.search_instruction_from_url
-                        )
-                    ]
-                if question_template == readmeai_template:
-                    readmeai_tool = ReadmeAI(url=url,project_name=proj_name)
-                    tools = [
-                        Tool(
-                            name="SHELL",
-                            description=shell.execute_command.__doc__,
-                            func=shell.execute_command
-                        ),
-                        Tool(
-                            name="READMEAI",
-                            description=readmeai_tool.generate_readme_and_return_instructions.__doc__,
-                            func=readmeai_tool.generate_readme_and_return_instructions
-                        )
-                    ]
+                GetIns = CompileNavigator(local_path=local_path, project_name=proj_name)
+                Dis = ErrorSolver(project_name=proj_name)
+                tools = [
+                    Tool(
+                        name="Shell",
+                        description=shell.execute_command.__doc__,
+                        func=shell.execute_command
+                    ),
+                    Tool(
+                        name="CompileNavigator",
+                        description=GetIns.get_instructions.__doc__,
+                        func=GetIns.get_instructions
+                    ),
+                    Tool(
+                        name="ErrorSolver",
+                        description=Dis.discussion.__doc__,
+                        func=Dis.discussion
+                    )
+                ]
                 # define agent
                 agent = create_react_agent(llm=llm,tools=tools,prompt=prompt)
                 agent_executor = CompilationAgentExecutor(agent=agent,tools=tools,verbose=True,return_intermediate_steps=True,max_iterations=30,handle_parsing_errors=True)
@@ -263,7 +113,6 @@ def start_compile(dataset_base_path,clean_copied_project,download_proxy,strict_c
                             }
                             if not res["output"]:
                                 res["output"] = res["final_answer"]
-                            # print(step)
                     checker_ok = is_compiled(local_path, files, strict=strict_checker)
                     checker_ok = f"{str(checker_ok)}(strict={str(strict_checker)})"
                 except Exception as e:
@@ -280,34 +129,20 @@ def start_compile(dataset_base_path,clean_copied_project,download_proxy,strict_c
 
                 logging.info("[-] Project {}, model result: {}, checker result: {}, path: {}".format(proj_name,model_ok,checker_ok,local_path))
 
-                if clean_copied_project:
-                    logging.info(f"[+] Clear project compilation path {local_path}")
+                # if clean_copied_project:
+                #     logging.info(f"[+] Clear project compilation path {local_path}")
                     # shutil.rmtree(local_path)
-                    cmd = f"echo {PASSWORD} | sudo -S rm -rf {local_path}"
-                    subprocess.run(cmd,shell=True)
+                    # cmd = f"echo {PASSWORD} | sudo -S rm -rf {local_path}"
+                    # cmd = f'rm -rf {local_path}'
+                    # subprocess.run(cmd,shell=True)
 
-                if question_template == model_decision_template:
-                    tools_log = {
-                        "shell":shell.logger,
-                        "get_instructions":get_ins.logger,
-                        "reconcile":rec.logger
-                    }
+                tools_log = {
+                    "Shell":shell.logger,
+                    "CompileNavigator":GetIns.logger,
+                    "ErrorSolver":Dis.logger
+                }
 
-                if question_template == rag_template:
-                    tools_log = {
-                        "shell":shell.logger,
-                        "search_instruction_from_files":search_inst_tool.logger1,
-                        "search_url_from_files":search_inst_tool.logger2,
-                        "search_instruction_from_url":search_inst_tool.logger3
-                    }
-                
-                if question_template == readmeai_template:
-                    tools_log = {
-                        "shell":shell.logger,
-                        "search_instruction":readmeai_tool.logger
-                    }
-
-                save_logs(proj_name,question,tools,res,tools_log,local_path,checker_ok,model_ok)
+                save_logs(log_path,proj_name,question,tools,res,tools_log,local_path,checker_ok,model_ok)
 
             if checker_ok.startswith("False"):
                 continue
@@ -320,6 +155,8 @@ def start_compile(dataset_base_path,clean_copied_project,download_proxy,strict_c
               help='the path to the project file to be compiled')
 @click.option('-p', '--dataset_base_path', type=str, required=True, 
               help='the base path to store the projects need to compile')
+@click.option('-l', '--log_path', type=str, required=True,
+               help='the path to store the logs of compilation')
 @click.option('-c', '--clean_copied_project', type=bool, required=True, 
               help='whether to clear the new project path after the compilation, set to True when debugging')
 @click.option('-r', '--retry', type=int, default=0,
@@ -330,14 +167,14 @@ def start_compile(dataset_base_path,clean_copied_project,download_proxy,strict_c
               help='pass to git --config http.proxy=${}, e.g., socks5://127.0.0.1:29999, if do not set: do not use proxy')
 @click.option('--multi_process', type=bool, default=False, 
               help='whether to enable the multi-process to compile the projects')
-def main(json_path,dataset_base_path,clean_copied_project,download_proxy,strict_checker,retry,multi_process):
+def main(json_path,dataset_base_path,log_path,clean_copied_project,download_proxy,strict_checker,retry,multi_process):
     '''
     Compile the projects from source code with the help of AI assistant
     '''
     with open(json_path, 'r') as fp:
         projects = json.load(fp)
 
-    base_tuple=(dataset_base_path,clean_copied_project,download_proxy,strict_checker,retry)
+    base_tuple=(dataset_base_path,log_path,clean_copied_project,download_proxy,strict_checker,retry)
     if multi_process:
         args = [base_tuple+([project],) for project in projects]
         with multiprocessing.Pool(processes=len(projects)) as pool:
@@ -357,11 +194,12 @@ def main(json_path,dataset_base_path,clean_copied_project,download_proxy,strict_
                 merge_data.append(csv_data)
             # os.system(f"rm {file_path}")
 
-    with open(LOG_CSV_PATH,"a") as fp:
+    with open(f"{log_path}/statistics.csv","a") as fp:
         csv_writer = csv.writer(fp)
         for data in merge_data:
             for row in data:
                 csv_writer.writerow(row)
+
 
 
 if __name__ == "__main__":
